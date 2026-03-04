cfe9f3dc32f0860b8922f9526d480c91b9d79ee9
karthiksaide <karthiksaide19@gmail.com>
2026-03-04 17:29:13 +0530

    fix render port binding

--- app.py
+++ app.py
@@ -205,7 +205,5 @@ def help():
 
 
 if __name__ == "__main__":
-
-    port = int(os.environ.get("PORT",10000))
-
-    app.run(host="0.0.0.0",port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
